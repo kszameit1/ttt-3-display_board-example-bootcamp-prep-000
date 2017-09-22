5b53f4bc8525baac7e7ec602af640ac2f0445701
Karen Lowe <k.szameit@mac.com>
2017-09-22 13:49:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board
-  puts row_1 = "   |   |   " # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-  puts sep_1 = " -----------"
+  puts row_1 = "   |   |   "
+  puts sep_1 = "-----------" # Define a method display_board that prints a 3x3 Tic Tac Toe Board
   puts row_2 = "   |   |   "
-  puts sep_2 = " -----------"
+  puts sep_2 = "-----------"
   puts row_3 = "   |   |   "
 end# Define a method display_board that prints a 3x3 Tic Tac Toe Board
